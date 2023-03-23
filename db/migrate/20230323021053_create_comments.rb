8c1b370a974a0924dea42503b12a053f297abad3
tmtarpinian <tmtarpinian@gmail.com>
2023-03-22 23:05:15 -0400

    update seeds.rb

--- db/migrate/20230323021053_create_comments.rb
+++ db/migrate/20230323021053_create_comments.rb
@@ -2,6 +2,7 @@ class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
       t.string :body
+      t.integer :article_id
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,27 @@
 Article.destroy_all
 Comment.destroy_all
 
-10000.times do |index|
+50.times do |index|
   Article.create!(
     title: Faker::Lorem.sentence(word_count: 3, supplemental: false, random_words_to_add: 0).chop,
     body: Faker::Lorem.paragraph,
   )
 end
 
-p "Created #{Movie.count} movies"
+p "Created #{Article.count} articles"
+
+article_ids = Article.all.pluck(:id)
+
+article_ids.each do |article_id|
+  35.times do |index|
+    Comment.create!(
+      body: Faker::Lorem.paragraph,
+      article_id: article_id
+    )
+  end
+
+end
+
+p "Created #{Comment.count} comments"
+
+article_ids.slice(0, (article_ids.length/2.0).round).each { |id| Article.find(id).delete }
